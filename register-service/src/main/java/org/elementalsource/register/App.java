612847210a0e949a29651edccb33b503db57697c
Ravi Arora <51474437+imraviarora@users.noreply.github.com>
2020-10-04 10:48:05 +0530

    Register Service module commit

--- register-service/src/main/java/org/elementalsource/register/App.java
+++ register-service/src/main/java/org/elementalsource/register/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Register-Service!" );
+        System.out.println( "Hello I'm Register-Service!" );
     }
 }
